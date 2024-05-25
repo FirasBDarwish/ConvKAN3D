356aebba1f9c6086356d36901e03315221937ec9
FirasBDarwish <darwish.firas.b@gmail.com>
2024-05-25 09:12:01 +0400

    fixed pytest

--- tests/test_ConvKAN3D_efficient.py
+++ tests/test_ConvKAN3D_efficient.py
@@ -2,18 +2,18 @@
 
 import pytest
 from unittest.mock import patch
-from ConvKAN3D.ConvKAN3D import ConvKAN3D
+from ConvKAN3D.ConvKAN3D import effConvKAN3D
 import torch
 
 class Tests:
     """ Class defines tests. """
 
     def test_assertions(self):
-        conv = ConvKAN3D(in_channels=3, out_channels=3, kernel_size=3)
-        assert str(type(conv)) == "<class 'ConvKAN3D.ConvKAN3D.ConvKAN3D'>"
+        conv = effConvKAN3D(in_channels=3, out_channels=3, kernel_size=3)
+        assert str(type(conv)) == "<class 'ConvKAN3D.ConvKAN3D.effConvKAN3D'>"
 
     def test_pass(self):
-        conv = ConvKAN3D(in_channels=3, out_channels=3, kernel_size=3)
+        conv = effConvKAN3D(in_channels=3, out_channels=3, kernel_size=3)
         x = torch.randn((64,3,9,9,9))
         y = conv(x)
         assert str(type(y)) == "<class 'torch.Tensor'>"
