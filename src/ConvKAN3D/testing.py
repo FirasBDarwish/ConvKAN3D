3f92a9246d4565bd3594a1ce46c716d8129b7d54
FirasBDarwish <darwish.firas.b@gmail.com>
2024-05-25 09:06:44 +0400

    changed object name to be different from package

--- src/ConvKAN3D/testing.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from unittest.mock import patch
-from ConvKAN3D import ConvKAN3D
-import torch
-
-conv = ConvKAN3D(in_channels=3, out_channels=3, kernel_size=3)
-x = torch.randn((64,3,9,9,9))
-y = conv(x)
-print(y.shape)
